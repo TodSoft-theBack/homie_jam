8ffa53a43c72fcb6ffd219c941345aff1348809b
Todor <63552782+TodSoft-theBack@users.noreply.github.com>
2023-04-21 01:59:14 +0000

    The site theme now changes based on time of day

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,27 @@
 import { createStore } from 'vuex'
 
+function IsDayTime() {
+  const hours = (new Date()).getHours();
+  return (hours >= 6 && hours < 18);
+}
+
+function CurrentTheme(){
+  return !IsDayTime();
+}
+
 export default createStore({
   state: {
-    SelectedDarkTheme: true,
+    CurrentTheme: CurrentTheme(),
     logged_in: false,
     IsOn:false,
   },
   getters: {
-    GetCurrentTheme: (state) => state.SelectedDarkTheme,
+    GetCurrentTheme: (state) => state.CurrentTheme,
     GetOnStatus: (state) => state.IsOn,
     CheckForLoggedInUser: (state) => state.logged_in
   },
   mutations: {
-    ChangeTheme: (state) => state.SelectedDarkTheme = !state.SelectedDarkTheme,
+    ChangeTheme: (state) => state.CurrentTheme = !state.CurrentTheme,
     ChangeStatus: (state) => state.IsOn = !state.IsOn
   },
   actions: {
@@ -23,6 +32,8 @@ export default createStore({
       commit('ChangeStatus')
     }
   },
-  modules: {
+  modules: 
+  {
+    
   }
 })
